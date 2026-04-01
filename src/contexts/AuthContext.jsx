de72cb651253fdaf491c94322af33208cdf2bbef
vaibhavR02 <vaibhavrandale800@gmail.com>
2026-04-02 00:44:47 +0530

    fix: update Axios baseURL to use production endpoint

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -6,8 +6,8 @@ const AuthContext = createContext();
 
 // ── Axios instance ─────────────────────────────────────
 export const api = axios.create({
-  // baseURL: "https://catering-backend-qffz.onrender.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://catering-backend-qffz.onrender.com",
+  // baseURL: "http://localhost:5000",
   // baseURL: [
   //   "http://localhost:5000",
   //   "https://catering-backend-qffz.onrender.com",
